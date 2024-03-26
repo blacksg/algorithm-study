9fb122d3baecd790a3420ee9aece265ed2c5bed8
blacksg <sagwa.nth@gmail.com>
2024-03-27 03:05:16 +0900

    ♻️ 풀이 코드 리팩터링 - [백준] #11497 통나무 건너뛰기 (Java)   - `Math.abs()` 호출 제거하여 실행 시간 단축   - 주석 수정

--- src/main/java/algorithm/sorting/BOJ11497.java
+++ src/main/java/algorithm/sorting/BOJ11497.java
@@ -1,9 +1,7 @@
-package sorting;
+package algorithm.sorting;
 
 import java.io.*;
-import java.util.*;
-
-import static java.lang.Math.*;
+import java.util.Arrays;
 
 /**
  * @문제 <a href="https://www.acmicpc.net/problem/11497">통나무 건너뛰기</a>
@@ -12,39 +10,31 @@ import static java.lang.Math.*;
  * @알고리즘 정렬, 그리디
  */
 class BOJ11497 {
-
+    
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder builder = new StringBuilder();
-
         int testCaseCount = Integer.parseInt(reader.readLine());
-
+        StringBuilder builder = new StringBuilder();
         for (int i = 0; i < testCaseCount; i++) {
             int logCount = Integer.parseInt(reader.readLine());
-            StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
-
-            // 통나무들의 높이를 배열에 저장하고 오름차순 정렬한다.
+            String[] strings = reader.readLine().split(" ");
             int[] logs = new int[logCount];
             for (int j = 0; j < logCount; j++) {
-                logs[j] = Integer.parseInt(tokenizer.nextToken());
+                logs[j] = Integer.parseInt(strings[j]);
             }
+            // 통나무의 높이를 오름차순 정렬
             Arrays.sort(logs);
-
-            // 1번재 통나무는 2번째 및 3번째 통나무와 연결한다.
-            int difficulty = max(abs(logs[0] - logs[1]), abs(logs[0] - logs[2]));
-
-            // 2번째부터 (n-2)번째 통나무까지는 두 차례 뒤의 통나무와 연결한다.
+            // 1번째와 2번째, 1번째와 3번째 연결
+            int answer = Math.max(logs[1] - logs[0], logs[2] - logs[0]);
+            // 마지막과 그 이전 통나무 연결
+            answer = Math.max(answer, logs[logCount - 1] - logs[logCount - 2]);
+            // 중간의 통나무들은 2개 간격으로 연결
             for (int j = 1; j < logCount - 2; j++) {
-                difficulty = max(difficulty, abs(logs[j] - logs[j + 2]));
+                answer = Math.max(answer, logs[j + 2] - logs[j]);
             }
-
-            // (n-1)번째 통나무와 n번째 통나무를 연결한다.
-            difficulty = max(difficulty, abs(logs[logCount - 2] - logs[logCount - 1]));
-
-            builder.append(difficulty).append("\n");
+            builder.append(answer).append('\n');
         }
-
         writer.write(builder.toString());
         reader.close();
         writer.close();
